a4e17faaa3d3f8cfb00910744ee7d2e8b675fdb9
Akib Ahmed <akibahmed7360@gmail.com>
2022-08-22 23:42:11 +0600

    added homePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './pages/home';
+
+
 
 function App() {
   return (
     <div className="App">
-     <p>Hello I am Akib Ahmed</p>
+     <Home/>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SignInBtn(){
+    return <div className="signInBtn">
+        <p>Sign in with Google</p>
+    </div>
+} 
