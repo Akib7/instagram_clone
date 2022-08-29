895999627ce800a99bed6f32ec9a3919a705a8c4
Akib Ahmed <akibahmed7360@gmail.com>
2022-08-30 00:42:02 +0600

    trying to fetch data from the firebase firestore database:

--- src/containers/feed/feed.js
+++ src/containers/feed/feed.js
@@ -25,7 +25,7 @@ export default function Feed() {
       <Post
         username="Akib"
         profileUrl="https://lh3.googleusercontent.com/a/AItbvmnQFwD5jPrhFMGmobIS62-fX2HRj7tp4sIlnRDVOgI=s96-c"
-        photoURL="https://firebasestorage.googleapis.com/v0/b/react-insta-1d4f3.appspot.com/o/images%2FdjSZvQNDln.jpg?alt=media&token=f40ff8e2-e02f-48f7-a717-21b41a095264"
+        photoURL="https://images7.alphacoders.com/310/310795.jpg"
         caption="Hey, this is a new post"
       />
 
